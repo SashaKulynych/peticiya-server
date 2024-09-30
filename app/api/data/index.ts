3e507f6639b4642e5a56cf265983fc8ab25dbc91
VRealSoft <78587110+vreal-soft@users.noreply.github.com>
2024-09-30 23:47:44 +0300

    update

--- app/api/data/index.ts
+++ app/api/data/index.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 data(router)
 
 router.get('/', (req: any, res: any) => {
-    res.send('server is up and running');
+    res.end('server is up and running');
 });
 
 export default router;
