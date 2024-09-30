98a1d20f09b6007e0d7eea8fc0e8aeffb80e2616
VRealSoft <78587110+vreal-soft@users.noreply.github.com>
2024-09-30 23:08:42 +0300

    update

--- src/router/data.ts
+++ src/router/data.ts
@@ -39,65 +39,69 @@ export const getData = async (data: {
 
 export default (router: any) => {
     router.get(link, (req: any, res: any) => {
-        const arr: IData = [...cachedData];
-        let pageNumber = 1;
-        const onFinish = () => {
-            setData(arr)
-            console.log({ arr })
-            res.send(arr);
-        };
-        const init = async () => {
-            const response = await getData({
-                petitionId: 234334,
-                pageNumber
-            });
-            console.log({ pageNumber, response })
-            if (response) {
-                const dom = (new jsdom.JSDOM(response.table_html)).window.document;
-                let finish = false;
-                const tableExist = dom.querySelector('.table_row');
-                if (tableExist) {
-                    const elements = dom.querySelectorAll('.table_row');
-                    for (let index = 0; index <= elements.length; index++) {
-                        const element = elements[index];
-                        if (element) {
-                            const number = element.querySelector('.number')?.innerHTML || '';
-                            const name = element.querySelector('.name')?.innerHTML || '';
-                            const date = element.querySelector('.date')?.innerHTML || '';
-                            const newItem = {
-                                number,
-                                name,
-                                date
-                            };
-                            const i = arr.findIndex((v) => v.date === date);
-                            if (i !== -1) {
-                                const alreadyExist = arr[i].data.find(
-                                    (v) => v.name === newItem.name
-                                );
-                                if (alreadyExist) {
-                                    finish = true;
+        try {
+            const arr: IData = [...cachedData];
+            let pageNumber = 1;
+            const onFinish = () => {
+                setData(arr)
+                console.log({ arr })
+                res.send({ success: true, data: arr });
+            };
+            const init = async () => {
+                const response = await getData({
+                    petitionId: 234334,
+                    pageNumber
+                });
+                console.log({ pageNumber, response })
+                if (response) {
+                    const dom = (new jsdom.JSDOM(response.table_html)).window.document;
+                    let finish = false;
+                    const tableExist = dom.querySelector('.table_row');
+                    if (tableExist) {
+                        const elements = dom.querySelectorAll('.table_row');
+                        for (let index = 0; index <= elements.length; index++) {
+                            const element = elements[index];
+                            if (element) {
+                                const number = element.querySelector('.number')?.innerHTML || '';
+                                const name = element.querySelector('.name')?.innerHTML || '';
+                                const date = element.querySelector('.date')?.innerHTML || '';
+                                const newItem = {
+                                    number,
+                                    name,
+                                    date
+                                };
+                                const i = arr.findIndex((v) => v.date === date);
+                                if (i !== -1) {
+                                    const alreadyExist = arr[i].data.find(
+                                        (v) => v.name === newItem.name
+                                    );
+                                    if (alreadyExist) {
+                                        finish = true;
+                                    } else {
+                                        arr[i].data.push(newItem);
+                                    }
                                 } else {
-                                    arr[i].data.push(newItem);
+                                    arr.unshift({
+                                        date,
+                                        data: [newItem]
+                                    });
                                 }
-                            } else {
-                                arr.unshift({
-                                    date,
-                                    data: [newItem]
-                                });
                             }
                         }
-                    }
-                    if (finish) {
-                        onFinish();
+                        if (finish) {
+                            onFinish();
+                        } else {
+                            pageNumber++;
+                            init();
+                        }
                     } else {
-                        pageNumber++;
-                        init();
+                        onFinish();
                     }
-                } else {
-                    onFinish();
                 }
-            }
-        };
-        init();
+            };
+            init();
+        } catch (e) {
+            res.send({ success: false, data: e });
+        }
     });
 } 
