bd80dfe3a3601239f64249557006bbc0500180f8
VRealSoft <78587110+vreal-soft@users.noreply.github.com>
2024-10-01 12:45:20 +0300

    update

--- app/utils.ts
+++ app/utils.ts
@@ -1,15 +1,16 @@
 import { IData } from "./api/data/data";
+import { cachedDataMain } from "./sources/CachedData";
 
 
 const fs = require('fs');
 
-export let cachedData: IData = []
+export let cachedData: IData = [...cachedDataMain]
 
 export const getData = () => {
     try {
-        const file = JSON.parse(fs.readFileSync('./app/sources/data.json', 'utf8'));
-        cachedData = file
-        return file
+        // const file = JSON.parse(fs.readFileSync('./app/sources/data.json', 'utf8'));
+        // cachedData = file
+        return cachedData
     } catch (e) {
         console.log({ e })
         return 'Error'
@@ -19,7 +20,7 @@ export const getData = () => {
 export const setData = (data: IData) => {
     try {
         cachedData = data
-        fs.writeFile('myjsonfile.json', JSON.stringify(data), 'utf8');
+        // fs.writeFile('myjsonfile.json', JSON.stringify(data), 'utf8');
     } catch (e) {
         console.log({ e })
     }
